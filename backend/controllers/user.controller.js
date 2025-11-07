ea8ab874220b08c99e0726cc7d3424fa92af1ca8
ManojM-656565 <137311406+ManojM-656565@users.noreply.github.com>
2025-11-07 13:46:28 +0530

    feat:backend logic for login and register created

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,82 @@
+const userModel = require("../models/user.model");
+const bcrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+const register=async()=>{
+    const{name,password,email,role}=req.body;
+    if(!name||!password||!email||!role){
+       return res.status(400).json({message:"Fields are missing"});
+    }
+
+    try{
+        const exist=userModel.findOne({email});
+        if(exist){
+            return res.status(400).json({message:"Email already exists"});
+        }
+        const hashed=await bcrypt.hash(password,10);
+        const user=await userModel.create({name,email,role,password:hashed})
+
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES_IN});
+        
+        res.cookie("token",token,{
+            httpOnly:true,
+            sameSite:"strict",
+            secure:process.env.NODE_ENV==="production",
+            maxAge:24*60*60*1000
+        })
+        res.status(201).json({
+            message:"User created successfully",
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role,
+            }
+        })
+
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message:"Internal server error"});
+    }
+
+}
+
+const login=async()=>{
+    const {email,password}=req.body;
+    try{
+        const user=userModel.findOne({email});
+        if(!user){
+            res.status(401).json({message:"Invalid user credentials"});
+        }
+        const ok=await bcrypt.compare(password,user.password);
+        if(!ok){
+            res.status(401).json({message:"Invaild credentials"});
+        }
+        const token=jwt.sign({id:user._id},process.env.JWT_SECRET,{
+            expiresIn:process.env.JWT_EXPIRES_IN
+        });
+        res.cookie("token",token,{
+            sameSite:"strict",
+            httpOnly:true,
+            secure:process.env.NODE_ENV==="production",
+            maxAge:24*60*60*1000
+        })
+        res.status(201).json({
+            message:"User Logged In successfully",
+            user:{
+                name:user.name,
+                email:user.email,
+                id:user._id,
+                role:user.role
+            }
+
+        })
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message:"Internal server error"});
+    }
+}
+
+module.exports={register}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express=require('express')
-const dotenv=require('dotenv')
+const dotenv=require('dotenv');
+const { connectDB } = require('./lib/db');
 
 dotenv.config()
 
 const app=express()
+
+app.use(express.json())
+
+app.use("/api/auth",authRoutes);
+
+
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
     console.log(`Server is running in port ${PORT}`);
+    connectDB();
 })
